f672000ab4048b14b069dfefe0f1d63ee2ee4ce8
mrossard <manuel.rossard@u-bordeaux.fr>
2025-12-03 08:57:53 +0100

    2025 day 3

---
+++ 2025/Day3/solve.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$banks = file($argv[1], FILE_IGNORE_NEW_LINES);
+
+$numberOfBatteries = $argv[2] ?? 2;
+
+function findMax(string $bank): int
+{
+    $maxValue = $bank
+        |> str_split(...)
+        |> max(...);
+
+    return strpos($bank, ''.$maxValue);
+}
+
+$result = $banks
+    |> function($banks) use ($numberOfBatteries){
+            return array_map(
+                callback: function(string $bank) use($numberOfBatteries) {
+                    $currentChar = 0;
+                    $previousPosition = -1;
+                    $chars = [];
+                    while($currentChar < $numberOfBatteries) {
+                        $length = 0 - ($numberOfBatteries - $currentChar) + 1;
+                        if($length == 0){
+                            $length = null;
+                        }
+                        $maxChar = findMax(
+                            substr(
+                                $bank,
+                                $previousPosition + 1,
+                                $length
+                            )
+                            ) + $previousPosition + 1;
+                        $chars[] = $bank[$maxChar];
+                        $currentChar++;
+                        $previousPosition = $maxChar;
+                    }
+                    return (int) implode('', $chars);
+                },
+                array: $banks
+            );
+    }
+    |> array_sum(...);
+
+echo 'part 2 : ', $result, PHP_EOL;
