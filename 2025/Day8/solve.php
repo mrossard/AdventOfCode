60fd05362a19d9f4d08feca18069f5a663d48d62
mrossard <manuel.rossard@u-bordeaux.fr>
2025-12-08 09:38:17 +0100

    2025 day 8 - light cleanup

--- 2025/Day8/solve.php
+++ 2025/Day8/solve.php
@@ -9,31 +9,29 @@ $boxes = file($argv[1], FILE_IGNORE_NEW_LINES)
         );
     });
 
-
+// calcul des distances, tri croissant
 $distances = [];
 
 for ($first = 0; $first < count($boxes) - 1; $first++) {
     for($second = $first + 1; $second < count($boxes); $second++) {
-        $distances[$first.'->'.$second] = ($boxes[$first][0] - $boxes[$second][0]) * ($boxes[$first][0] - $boxes[$second][0])
-            + ($boxes[$first][1] - $boxes[$second][1]) * ($boxes[$first][1] - $boxes[$second][1])
-            + ($boxes[$first][2] - $boxes[$second][2]) * ($boxes[$first][2] - $boxes[$second][2]);
+        $distances[$first.'->'.$second] = ($boxes[$first][0] - $boxes[$second][0]) ** 2
+            + ($boxes[$first][1] - $boxes[$second][1]) ** 2
+            + ($boxes[$first][2] - $boxes[$second][2]) ** 2;
     }
 }
-
 asort($distances);
 
-$circuits = [];
-
+// calcul des circuits en fonction du nombre de connexions demandées
 $connectionsToMake = $argv[2] ?? PHP_INT_MAX;
-
 $relevantDistances = array_slice($distances, 0, $connectionsToMake);
 
+$circuits = [];
 foreach($relevantDistances as $boxIds => $distance) {
     if(count($circuits) == 1 && count(array_first($circuits)) === count($boxes)){
         break;
     }
     [$first, $second] = explode('->', $boxIds);
-    $previousBoxes = [$first, $second];
+    $previousBoxes = [$first, $second]; //pour la partie 2, flemme de mettre la condition de sortie au bon endroit
     $firstCircuit = null;
     $secondCircuit = null;
     foreach($circuits as $circuitId => $circuit){
@@ -58,27 +56,22 @@ foreach($relevantDistances as $boxIds => $distance) {
         $circuits[$firstCircuit][] = $second;
         continue;
     }
-    //on merge les deux
     if($firstCircuit === $secondCircuit){
         continue;
     }
+    //on merge les deux
     $circuits[$firstCircuit] = array_merge($circuits[$firstCircuit], $circuits[$secondCircuit]);
     unset($circuits[$secondCircuit]);
 }
 
+if($connectionsToMake !== PHP_INT_MAX){
+    $circuitLengths = array_map(count(...), $circuits);
+    rsort($circuitLengths);
+    $top3 = array_slice($circuitLengths, 0, 3)
+            |> array_product(...);
 
-$circuitLengths = array_map(count(...), $circuits);
-
-rsort($circuitLengths);
-
-$top3 = array_slice($circuitLengths, 0, 3)
-    |> ( function($lengths) {
-        return array_reduce(
-            $lengths,
-            fn($carry, $length) => $carry * $length,
-            1
-        );
-    });
-
-echo 'part 1 result: ' . $top3 . PHP_EOL;
-echo 'part 2 result: ' . $boxes[$previousBoxes[0]][0] * $boxes[$previousBoxes[1]][0]  . PHP_EOL;
+    echo 'part 1 result: ' . $top3 . PHP_EOL;
+}
+else {
+    echo 'part 2 result: '.$boxes[$previousBoxes[0]][0] * $boxes[$previousBoxes[1]][0].PHP_EOL;
+}
